f4fbe1afc2908c51a972b778ecd116c8512c6b99
Timilehin Toba <76178289+SirTimi@users.noreply.github.com>
2026-06-04 15:46:52 +0100

    remove accidental api app module

--- apps/api/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdminGovernanceModule } from './admin/admin-governance.module.js';
-
-@Module({
-  imports: [AdminGovernanceModule],
-})
-export class AppModule {}
