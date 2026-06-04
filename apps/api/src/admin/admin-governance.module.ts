5547950f925350ea173ed7e0896d882c71847da0
Timilehin Toba <76178289+SirTimi@users.noreply.github.com>
2026-06-04 15:48:25 +0100

    remove accidental api governance module

--- apps/api/src/admin/admin-governance.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdminGovernanceController } from './admin-governance.controller.js';
-import { AdminPermissionGuard } from './permissions/admin-permission.guard.js';
-
-@Module({
-  controllers: [AdminGovernanceController],
-  providers: [AdminPermissionGuard],
-})
-export class AdminGovernanceModule {}
