a22b5038cb523c0b05e46e68dc32eaec011ac729
Timilehin Toba <76178289+SirTimi@users.noreply.github.com>
2026-09-20 08:26:31 +0100

    phase8: configure financial ledger cutover

--- apps/api/src/config/env.validation.ts
+++ apps/api/src/config/env.validation.ts
@@ -89,6 +89,7 @@ export const envValidationSchema = Joi.object({
   TREASURY_BANK_REFERENCE: Joi.string().allow('').default(''),
   TREASURY_BANK_CODE: Joi.string().allow('').default(''),
   TREASURY_BANK_ACCOUNT_LAST4: Joi.string().allow('').pattern(/^\d{4}$/).default(''),
+  FINANCIAL_LEDGER_CUTOVER_AT: Joi.string().allow('').default(''),
 
   WALLET_FUNDING_MIN_NGN: Joi.number().integer().min(1).default(100),
   WALLET_FUNDING_MAX_NGN: Joi.number().integer().min(100).default(500000),
